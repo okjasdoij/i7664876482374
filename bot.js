e7f8c71e2a024ebe8c9f0ff379aedb43c882db9a
okjasdoij <45161348+okjasdoij@users.noreply.github.com>
2018-11-23 09:10:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -142,6 +142,31 @@ acc15.login(process.env.ACC15_TOKEN);
 
 
 
+const acc16 = new Discord.Client();
+acc16.on('ready', () => {
+    console.log('Account 16 Ready!');
+    setInterval(() => {
+        acc16.channels.find(c => c.id == channel).send(randomWords[Math.floor(Math.random() * randomWords.length)]);
+    }, 500);
+    setInterval(() => {
+       acc16.channels.find(c => c.id == channel).send('#daily');
+    }, 86403000);
+});
+acc16.on('message', message => {
+    if(!pepole.includes(message.author.id)) return;
+    if(message.content.toLowerCase().split(' ')[0] == prefix.acc16 + 'say') {
+        if(!message.content.split(' ').slice(1).join(' ')) return message.channel.send('What i say?');
+        message.channel.send(message.content.split(' ').slice(1).join(' '));
+    }
+    if(message.content.toLowerCase().split(' ')[0] == '!allsay') {
+        if(!message.content.split(' ').slice(1).join(' ')) return;
+        message.channel.send(message.content.split(' ').slice(1).join(' '));
+    }
+});
+acc16.login(process.env.ACC16_TOKEN);
+
+
+
 const acc17 = new Discord.Client();
 acc17.on('ready', () => {
     console.log('Account 17 Ready!');
